59ae84bd8229972a181bc70daa09745cacbd5ee8
Althaf <althafrifath@gmail.com>
2024-02-24 22:53:15 +0530

    props, propTypes and defaultProps

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
-import Card from "./Card";
+import Student from "./Student";
 
 function App() {
   return (
     <>
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
+      <Student name="Althaf" age={20} isStudent={true}/>
+      <Student name="Arun" age={21} isStudent={true}/>
+      <Student name="Anand" age={22} isStudent={false}/>
+      <Student name="Aruna" age={23} isStudent={true}/>
+      <Student/>
     </>
   );
 }
